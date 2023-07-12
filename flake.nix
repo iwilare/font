0108e7806a5525cd1d95df38d698be19de2a470a
iwilare <iwilare@gmail.com>
2023-07-12 18:17:36 +0300

    Use flake-compat

--- flake.nix
+++ flake.nix
@@ -3,26 +3,43 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        defaultPackage = pkgs.symlinkJoin {
-          name = "iwifonts-1.0";
-          paths = builtins.attrValues
-            self.packages.${system}; # Add font derivation names here
-        };
-        packages.commitmono = pkgs.stdenvNoCC.mkDerivation {
-          name = "commitmono-font";
-          dontInstall = true;
-          src = "CommitMono.zip";
-          installPhase = ''
-            mkdir -p $out/share/fonts
-            cp -R $src $out/share/fonts/opentype/
-          '';
-          meta = { description = "Commit mono derivation 2023-07-12."; };
-        };
+          commitmono = pkgs.stdenvNoCC.mkDerivation {
+              name = "commitmono-font";
+              src = ./.;
+              noConfig = true;
+              buildInputs = [
+                pkgs.unzip
+              ];
+              installPhase = ''
+                mkdir fonts
+                unzip $src/CommitMono.zip -d fonts
+                mkdir -p $out/share/fonts
+                cp -R fonts/ $out/share/fonts/opentype/
+              '';
+              meta = { description = "Commit mono derivation 2023-07-12."; };
+          }; in {
+            packages = {
+              inherit commitmono;
+              default = commitmono;
+            };
+          }
+    );
+}
+
+
+        # defaultPackage = pkgs.symlinkJoin {
+        #   name = "iwifonts-1.0";
+        #   paths = builtins.attrValues
+        #     self.packages.${system}; # Add font derivation names here
+        # };
 
         # packages.gillsans = pkgs.stdenvNoCC.mkDerivation {
         #   name = "gillsans-font";
@@ -55,5 +72,3 @@
         #   '';
         #   meta = { description = "The Palatino Font Family derivation."; };
         # };
-      });
-}
