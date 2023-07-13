30d8f0219f3653e15f5d49c3a2b2af8510df2741
iwilare <iwilare@gmail.com>
2023-07-13 23:49:15 +0300

    Merge both

--- flake.nix
+++ flake.nix
@@ -29,10 +29,29 @@
                 done
               '';
               meta = { description = "DejaVu Sans Code with complete Nerd Font patching (2023-07-13)"; };
+          };
+          dejavucode-nerd-font = pkgs.stdenvNoCC.mkDerivation {
+              name = "dejavucode-nerd-font";
+              src = pkgs.fetchzip {
+                url = "https://github.com/SSNikolaevich/DejaVuSansCode/releases/download/v1.2.2/dejavu-code-ttf-1.2.2.zip";
+                sha256 = "sha256-208QBXkeQIMwawCDhVG4DNqlGh5GYfhTNJybzMZhE/4=";
+              };
+              noConfig = true;
+              buildInputs = [
+                pkgs.unzip
+              ];
+              installPhase = ''
+                cd ttf
+                mkdir -p $out/share/fonts/truetype
+                for f in *; do
+                  ${pkgs.nerd-font-patcher}/bin/nerd-font-patcher -c $f -o $out/share/fonts/truetype/
+                done
+              '';
+              meta = { description = "DejaVu with complete Nerd Font patching (2023-07-13)"; };
           }; in {
             packages = {
               inherit dejavusansmonocode-nerd-font;
-              default = dejavusansmonocode-nerd-font;
+              inherit dejavucode-nerd-font;
             };
           }
     );
