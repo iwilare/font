3469407c7bc3dfd60e2734565f1e460713bfe5d8
iwilare <iwilare@gmail.com>
2023-07-13 17:12:20 +0300

    DejaVuSansMonoCode

--- flake.nix
+++ flake.nix
@@ -11,28 +11,28 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
-          dejavucode-nerd-font = pkgs.stdenvNoCC.mkDerivation {
-              name = "dejavucode-nerd-font";
+          dejavusansmonocode-nerd-font = pkgs.stdenvNoCC.mkDerivation {
+              name = "dejavusansmonocode-nerd-font";
               src = pkgs.fetchzip {
-                url = "https://github.com/SSNikolaevich/DejaVuSansCode/releases/download/v1.2.2/dejavu-code-ttf-1.2.2.zip";
-                sha256 = "sha256-208QBXkeQIMwawCDhVG4DNqlGh5GYfhTNJybzMZhE/4=";
+                url = "https://github.com/mdschweda/dejavusansmonocode/releases/download/v1.1/DejaVuSansMonoCode.zip";
+                sha256 = "sha256-MJPTJoKDzCEgbrYZ/66fHkLSpO34RDF5Jix+1n2Xfck=";
+                stripRoot = false;
               };
               noConfig = true;
               buildInputs = [
                 pkgs.unzip
               ];
               installPhase = ''
-                cd ttf
                 mkdir -p $out/share/fonts/truetype
-                for f in *; do
-                  ${pkgs.nerd-font-patcher}/bin/nerd-font-patcher -c --progressbars $f -o $out/share/fonts/truetype/
+                for f in *.ttf; do
+                  ${pkgs.nerd-font-patcher}/bin/nerd-font-patcher -c --noprogressbars $f -o $out/share/fonts/truetype/
                 done
               '';
               meta = { description = "DejaVu Sans Code with complete Nerd Font patching (2023-07-13)"; };
           }; in {
             packages = {
-              inherit dejavucode-nerd-font;
-              default = dejavucode-nerd-font;
+              inherit dejavusansmonocode-nerd-font;
+              default = dejavusansmonocode-nerd-font;
             };
           }
     );
